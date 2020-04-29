5ebd930c89404aa60b260f5e390ce015c54c161a
Mason Ashment <masonashment@gmail.com>
2020-04-29 15:30:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/todotogether/TaskListFragment.java
@@ -0,0 +1,37 @@
+package com.example.todotogether;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TaskListFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_task_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = view.findViewById(R.id.recyclerViewTasks);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
